c83c4e067af8ce7cb8cc8736e38ba3a941e50fe2
Daddywilbz <158806755+Daddywilbz@users.noreply.github.com>
2025-04-23 14:41:25 +0800

    Update DataStore.java

--- src/utility/DataStore.java
+++ src/utility/DataStore.java
@@ -22,12 +22,18 @@ public class DataStore {
         users.add(new Applicant("S1234567A", "password", "Alice", 35, "Single"));
         users.add(new Applicant("T7654321B", "password", "Bob", 40, "Married"));
         users.add(new Applicant("T2934242C", "password", "Robert", 34, "Single"));
+        users.add(new Applicant("T2304952Z", "password", "Charlie", 25, "Married"));
         users.add(new HDBOfficer("S1122334C", "password", "Officer Joe", 36, "Married"));
+        users.add(new HDBOfficer("S2390293D", "password", "Officer Amy", 41, "Married"));
+        users.add(new HDBOfficer("S2938742G", "password", "Officer Roy", 39, "Married"));
         users.add(new HDBManager("T9988776D", "password", "Manager Jane", 45, "Married"));
+        users.add(new HDBManager("S1653293K", "password", "Manager Michael", 52, "Married"));
 
         // Sample Project
-        HDBManager jane = (HDBManager) users.get(4);
+        HDBManager jane = (HDBManager) users.get(7);
+        HDBManager michael = (HDBManager) users.get(8);
         Project p1 = new Project("Acacia Breeze", "Yishun", jane, 2, 350000, 3, 450000, 10);
+        
 
         PrintStream originalOut = System.out;
         System.setOut(new PrintStream(OutputStream.nullOutputStream()));
@@ -36,10 +42,21 @@ public class DataStore {
         InputStream originalIn = System.in;
         System.setIn(fakeIn);
         p1.setDates();
+        projects.add(p1);
+
+        HDBManager michael = (HDBManager) users.get(8);
+        Project p2 = new Project("Green Valley", "Boon Lay", michael, 5, 300000, 10, 400000, 8);
+        
+        
+        String fakeDatesP2 = "1 4 2025 1 5 2025\n";
+        System.setIn(new ByteArrayInputStream(fakeDatesP2.getBytes()));
+        p2.setDates();
+        projects.add(p2);
+        
         System.setOut(originalOut);
         System.setIn(originalIn);
 
-        projects.add(p1);
+        
     }
 
 
