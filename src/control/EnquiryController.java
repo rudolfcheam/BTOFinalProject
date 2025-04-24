d2a2722df446a5d2c41751179fea30af61b24b0a
Yukino2711 <yangqirui0811@gmail.com>
2025-04-24 20:16:26 +0800

    Update EnquiryController.java

--- src/control/EnquiryController.java
+++ src/control/EnquiryController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class EnquiryController {
+public class EnquiryController implements EnquiryService {
     private Scanner scanner = new Scanner(System.in);
     ProjectController pc = new ProjectController();
 
