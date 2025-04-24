c413814b22968a65deb0068853d576fb28d83347
Yukino2711 <yangqirui0811@gmail.com>
2025-04-24 20:18:08 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import boundary.CLI;
 
 public class Main {
     public static void main(String[] args) {
-        CLI cli = new CLI();
+        ApplicationService appService = new ApplicationController();
+        AuthService authService = new AuthController();
+        EnquiryService enquiryService = new EnquiryController();
+        ProjectService projectService = new ProjectController();
+
+        CLI cli = new CLI(authService, projectService, appService, enquiryService);
         cli.start();
     }
 }
